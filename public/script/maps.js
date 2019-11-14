86a504e7f94cbb48eb1f0c0a4b0c0271b1f574b0
YoungJun <dudwns9073@nate.com>
2019-11-14 17:21:57 +0900

    router index, maps [UPDATE] index - favoriate router add   - favoriate 기능을 추가하여 즐겨찾기 버튼을 클릭 할 때 document[favirate]에 저장을 한다. - isfavoriate변수를 활용하여 즐겨찾기 버튼을 만들지를 정한다. maps - init -> setPosition으로 method 명 변경   - async가 맞지 않은 관계로 함수를 아직까지는 사용하지 않는다.  deprecated - setMarker 추가   -  검색 결과를 바탕으로 pin을 map에 매핑 작업한다.   - 'infowindow'를 활용하여 pin을 클릭할 때 정보를 볼 수 있도록 한다. - 정보는 오직 한 개만 볼 수 있도록 info.close()를 자동으로 호출한다.

--- public/script/maps.js
+++ public/script/maps.js
@@ -1,31 +1,78 @@
 var positions = [];
-
+var infobox;
+var map;
+var isfavorite;
 /**
  * 
  * @param {Array} results 
+ * @deprecated
  */
-function init(results) {
+function setPositions(results) {
   for (result of results) {
     positions.push({
-      lat: result.location[1],
-      lng: result.location[0],
+      lat: result.geometry.location.lat,
+      lng: result.geometry.location.lng,
       name: result.name,
-      id: result.placeId
+      id: result.place_id
     });
   }
 }
 
 function initMap() {
   var center = positions[0] || { lat: 37.540705, lng: 126.956764 };
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: center
   });
-  positions.forEach(function (pos) {
-    new google.maps.Marker({
-      position: { lat: pos.lat, lng: pos.lng },
+  setMarker(isfavorite);
+}
+
+function setMarker() {
+  if (!positions || positions.length == 0) return;
+
+  positions.forEach(function (position) {
+    var marker = new google.maps.Marker({
+      position: { lat: position.lat, lng: position.lng },
       map: map,
-      title: pos.name
+      title: position.name
     });
+
+    var div = document.createElement('div');
+    var h1 = document.createElement('h1');
+    h1.textContent = position.name;
+    
+    if(isfavoriate){
+      var button = document.createElement('button');
+      button.classList.add('btn');
+      button.textContent = '즐겨찾기 추가';
+  
+      button.onclick = function () {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          if (xhr.status === 200) {
+            location.href = '/';
+          }
+        };
+        xhr.open('POST', `/location/${position.id}/favorite`);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+          name: position.name,
+          lat: position.lat,
+          lng: position.lng
+        }));
+      };
+      div.appendChild(button);
+    }
+    
+    div.appendChild(h1);
+    var infowindow = new google.maps.InfoWindow({
+      content: div
+    });
+    marker.addListener('click', function () {
+      infobox.close();
+      infobox = infowindow;
+      infowindow.open(map, marker);
+    });
+    infobox = infowindow;
   });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,25 @@ const util = require('util');
 const googleMaps = require('@google/maps');
 
 const History = require('../schemas/history');
+const Favorite = require('../schemas/favorites');
 
 const router = express.Router();
 const googleMapsClient = googleMaps.createClient({
     key: process.env.PLACES_API_KEY,
 });
 
-router.get('/', (_req, res) => {
-    res.render('index', { Google_Key: process.env.PLACES_API_KEY });
+router.get('/', async (_req, res) => {
+    try{
+        const favorites = await Favorite.find({});
+        res.render('index', {
+            isfavoriate: false,
+            results : favorites,
+            Google_Key: process.env.PLACES_API_KEY 
+        })
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
 });
 
 router.get('/autocomplete/:query', (req, res, next) => {
@@ -40,9 +51,25 @@ router.get('/search/:query', async (req, res, next) => {
             title: `${req.params.query}결과`,
             results: response.json.results,
             Google_Key: process.env.PLACES_API_KEY,
+            isfavoriate: true,
             query: req.params.query,
         });
     } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/location/:id/favorite', async (req, res, next) => {
+    try{
+        const favoriate = new Favorite({
+            placeId: req.params.id,
+            name: req.body.name,
+            location: [req.body.lng, req.body.lat],
+        });
+        await favoriate.save();
+        res.status(200).send(favoriate);
+    }catch(error) {
         console.error(error);
         next(error);
     }
